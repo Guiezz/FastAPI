26ccd00f43ecb54d1f2b8639f5df50b6a5f6b626
Guilherme Bessa <128107879+Guiezz@users.noreply.github.com>
2024-01-05 18:43:10 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from product import Product
+
+from json_db import JsonDB
+
+from indb import generate_products
+
+
+app = FastAPI()
+
+productDB = JsonDB(path = './data/products.json')
+
+@app.get('/products')
+def get_products():
+    products = productDB.read()
+    return {"products": products}
+
+@app.post('/products')
+def create_product(product: Product):
+
+    productDB.insert(product)
+
+    return {"status": "inserted"}
